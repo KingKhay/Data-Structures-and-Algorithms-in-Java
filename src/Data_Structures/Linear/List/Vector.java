0ba869423fb625177dd39fd7295818c3c82be815
king_khay <ebenezerbuabeng60@gmail.com>
2022-01-31 13:23:57 +0200

    Added a review of Vectors

---
+++ src/Data_Structures/Linear/List/Vector.java
@@ -0,0 +1,14 @@
+package Data_Structures.Linear.List;
+
+public class Vector {
+    /*
+    * Vectors are essentially thread safe Arraylist
+    * The main differences between a java vector and arraylist is
+    * 1.Vectors are synchronised
+    * 2.Vectors contain more legacy methods that are not part of a collections' framework.//
+    *
+    * Some methods in Vectors are add, addAll, addElement, capacity, clear,clone, contains, elementAt,
+    * firstElement,get, InsertElementAt, isEmpty, lastIndexOf, lastElement,remove, removeAll,set,size....//
+    * */
+
+}
